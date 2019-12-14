7adbf1c574a5858740565a55e3161e03982327c3
Paul <yl7452ls@go.minnstate.edu>
2019-12-14 13:41:15 -0600

    Fixed issues with the file, just committing the changes

--- Recipe.py
+++ Recipe.py
@@ -1,4 +1,4 @@
-import requests, docx           # These are the libraries that are used in the files to create them in Python
+import docx, requests      # These are the libraries that are used in the files to create them in Python
 
 
 '''Down here, I will create the function that will get the taco API and find the specific parts to 
@@ -12,9 +12,13 @@ def recipe_print():
     mixin_data = data['mixin']
     base_layer_data = data['base_layer']
     shell_data = data['shell']
-
     document = docx.Document()
     document.add_paragraph('Random Taco Cookbook', 'Title')
-    document.add_picture('Omega Taco.jpg')
-    document.save('Taco Final')
+    document.add_picture('Omega Taco.jpg', width=docx.shared.Inches(3), height=docx.shared.Inches(3))
+    document.add_paragraph('Picture by: Jason Leung')
+
+
+    document.save('TacoFinish.docx')
+
+recipe_print()
 
