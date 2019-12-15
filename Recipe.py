e705c6c5eb5e30cd753dcf05c097af4740bfe300
Paul <yl7452ls@go.minnstate.edu>
2019-12-15 17:35:02 -0600

    Found the reason the recipes only printed "urlslugrecipe", had to add a recipe key to each section of the taco recipe.

--- Recipe.py
+++ Recipe.py
@@ -7,18 +7,23 @@ take to the Word Document that will be created.'''
 def recipe_print():
 
     data = requests.get('https://taco-1150.herokuapp.com/random/?full_taco=true').json()    # This is the API where I get the random taco recipes
-    seasoning_data = data['seasoning']
-    condiment_data = data['condiment']
-    mixin_data = data['mixin']
-    base_layer_data = data['base_layer']
-    shell_data = data['shell']
+    seasoning_data = data["seasoning"]['recipe']
+    condiment_data = data ['condiment']['recipe']
+    mixin_data = data ['mixin']['recipe']
+    base_layer_data = data ['base_layer']['recipe']
+    shell_data = data ['shell']['recipe']
+
+
     document = docx.Document()
     document.add_paragraph('Random Taco Cookbook', 'Title')
     document.add_picture('Omega Taco.jpg', width=docx.shared.Inches(3), height=docx.shared.Inches(3))
     document.add_paragraph('Picture by: Jason Leung')
+    document.add_paragraph('Recipes found at: https://taco-1150.herokuapp.com/random/?full_taco=true')
+    document.add_paragraph('Code created by: Paul Snowdey Jr.')
+    document.add_paragraph(seasoning_data, 'Title')
+    document.add_paragraph(mixin_data)
 
 
     document.save('TacoFinish.docx')
 
 recipe_print()
-
