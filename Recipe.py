8ee2e8a8f673a9317d9deae86efb3f49c8b9ec4d
yl7452ls <yl7452ls@go.minnstate.edu>
2019-12-18 11:45:43 -0600

    Added more comments to the code

--- Recipe.py
+++ Recipe.py
@@ -9,25 +9,25 @@ def recipe_print():
 
 
     data = requests.get('https://taco-1150.herokuapp.com/random/?full_taco=true').json()    # This is the API where I get the random taco recipes
-    seasoning_data = data["seasoning"]['recipe']    #T This is where I get the
-    seasoning_name = data["seasoning"]['name']
-    condiment_data = data['condiment']['recipe']
-    condiment_name = data['condiment']['name']
-    mixin_data = data['mixin']['recipe']
-    mixin_name = data['mixin']['name']
-    base_layer_data = data['base_layer']['recipe']
-    base_name = data['base_layer']['name']
-    shell_data = data['shell']['recipe']
-    shell_name = data['shell']['name']
+    seasoning_data = data["seasoning"]['recipe']    # Gathering the data for the seasoning
+    seasoning_name = data["seasoning"]['name']      # This is where I get just the name for the seasoning
+    condiment_data = data['condiment']['recipe']    # Gathering data for the condiments
+    condiment_name = data['condiment']['name']      # Getting the name of the condiments
+    mixin_data = data['mixin']['recipe']            # This is data for the mixing
+    mixin_name = data['mixin']['name']              # This is just the name for the mixin
+    base_layer_data = data['base_layer']['recipe']  # This is the data for the base layer
+    base_name = data['base_layer']['name']          # This is the name for the base layer
+    shell_data = data['shell']['recipe']            # Gathering data for the shell
+    shell_name = data['shell']['name']              # This is where I get the name of the shell
 
-
+    # This is where the document is made and where I piece each thing together
     document = docx.Document()
-    document.add_paragraph('Random Taco Cookbook', 'Title')
-    document.add_picture('Omega Tacos.jpg')
-    document.add_paragraph('Picture by: Jason Leung')
-    document.add_paragraph('Recipes found at: https://taco-1150.herokuapp.com/random/?full_taco=true')
+    document.add_paragraph('Random Taco Cookbook', 'Title')     # That is the name of the taco cookbook
+    document.add_picture('Omega Tacos.jpg')     # This is my edited image I received from Unsplash
+    document.add_paragraph('Picture by: Jason Leung')   # Citing the photographer of the image
+    document.add_paragraph('Recipes found at: https://taco-1150.herokuapp.com/random/?full_taco=true')  # Citing the API that creates the recipes
     document.add_paragraph('Code created by: Paul Snowdey Jr.')
-    document.add_page_break()
+    document.add_page_break()       # This is where the recipes will start
     document.add_paragraph(seasoning_name, 'Title')
     document.add_paragraph(seasoning_data)
     document.add_paragraph(mixin_name, 'Title')
@@ -42,4 +42,5 @@ def recipe_print():
 
     document.save('TacoFinish.docx')
 
-recipe_print()
+for taco in range(3):
+    recipe_print()
