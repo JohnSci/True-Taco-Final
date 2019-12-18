f0a4a52768573eb72ea03d0310c4a12312adceae
yl7452ls <yl7452ls@go.minnstate.edu>
2019-12-18 11:19:41 -0600

    Added page breaks and titled the names of the recipe.

--- Recipe.py
+++ Recipe.py
@@ -10,10 +10,15 @@ def recipe_print():
 
     data = requests.get('https://taco-1150.herokuapp.com/random/?full_taco=true').json()    # This is the API where I get the random taco recipes
     seasoning_data = data["seasoning"]['recipe']    #T This is where I get the
-    condiment_data = data ['condiment']['recipe']
-    mixin_data = data ['mixin']['recipe']
-    base_layer_data = data ['base_layer']['recipe']
-    shell_data = data ['shell']['recipe']
+    seasoning_name = data["seasoning"]['name']
+    condiment_data = data['condiment']['recipe']
+    condiment_name = data['condiment']['name']
+    mixin_data = data['mixin']['recipe']
+    mixin_name = data['mixin']['name']
+    base_layer_data = data['base_layer']['recipe']
+    base_name = data['base_layer']['name']
+    shell_data = data['shell']['recipe']
+    shell_name = data['shell']['name']
 
 
     document = docx.Document()
@@ -22,11 +27,17 @@ def recipe_print():
     document.add_paragraph('Picture by: Jason Leung')
     document.add_paragraph('Recipes found at: https://taco-1150.herokuapp.com/random/?full_taco=true')
     document.add_paragraph('Code created by: Paul Snowdey Jr.')
-    document.add_paragraph(seasoning_data, 'Title')
+    document.add_page_break()
+    document.add_paragraph(seasoning_name, 'Title')
+    document.add_paragraph(seasoning_data)
+    document.add_paragraph(mixin_name, 'Title')
     document.add_paragraph(mixin_data)
+    document.add_paragraph(base_name, 'Title')
     document.add_paragraph(base_layer_data)
+    document.add_paragraph(shell_name, 'Title')
     document.add_paragraph(shell_data)
-
+    document.add_page_break()
+    document.add_paragraph
 
 
     document.save('TacoFinish.docx')
