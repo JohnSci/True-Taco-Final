bdbeeab462b27627f348a6ad12a9de978c33f90e
yl7452ls <yl7452ls@go.minnstate.edu>
2019-12-19 16:15:24 -0600

    Adding more comments

--- Recipe.py
+++ Recipe.py
@@ -4,9 +4,10 @@ import docx, requests      # These are the libraries that are used in the files 
 '''Down here, I will create the function that will get the taco API and find the specific parts to 
 take to the Word Document that will be created.'''
 
+url = 'https://taco-1150.herokuapp.com/random/?full_taco=true'      ''' This is the URL of the API and 
+                                                                        how I will identify it in the code'''
+data = requests.get(url).json()    # This is the API where I get the random taco recipes
 
-
-data = requests.get('https://taco-1150.herokuapp.com/random/?full_taco=true').json()    # This is the API where I get the random taco recipes
 seasoning_data = data["seasoning"]['recipe']    # Gathering the data for the seasoning
 seasoning_name = data["seasoning"]['name']      # This is where I get just the name for the seasoning
 condiment_data = data['condiment']['recipe']    # Gathering data for the condiments
@@ -18,19 +19,24 @@ base_name = data['base_layer']['name']          # This is the name for the base 
 shell_data = data['shell']['recipe']            # Gathering data for the shell
 shell_name = data['shell']['name']              # This is where I get the name of the shell
 
-        # This is where the document is made and where I piece each thing together
 
-
-
+                                                    '''This upcoming section is where my code creates
+                                                    the Word document'''
 document = docx.Document()
 
 document.add_paragraph('Random Taco Cookbook', 'Title')     # That is the name of the taco cookbook
 document.add_picture('Omega Tacos.jpg')     # This is my edited image I received from Unsplash
 document.add_paragraph('Picture by: Jason Leung')   # Citing the photographer of the image
 document.add_paragraph('Recipes found at: https://taco-1150.herokuapp.com/random/?full_taco=true')  # Citing the API that creates the recipes
-document.add_paragraph('Code created by: Paul Snowdey Jr.')
+document.add_paragraph('Code created by: Paul Snowdey Jr.')         #Citing myself as creator of the code
 document.add_page_break()       # This is where the recipes will start
+
+                                                                            '''The code below is where I create a loop
+                                                                                for the taco recipes so the Word
+                                                                                document creates 3 different
+                                                                                recipes.'''
 for taco in range(3):
+    document.add_paragraph(f'Taco {taco + 1}', 'Heading6')
     document.add_paragraph(seasoning_name, 'Title')
     document.add_paragraph(seasoning_data)
     document.add_paragraph(mixin_name, 'Title')
@@ -40,4 +46,5 @@ for taco in range(3):
     document.add_paragraph(shell_name, 'Title')
     document.add_paragraph(shell_data)
     document.add_page_break()
+
 document.save('TacoFinish.docx')
